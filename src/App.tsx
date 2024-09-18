33cf302ebeb9766b4077a97d1568fe392f05f801
working05 <hty050216@gmail.com>
2024-09-18 10:19:06 +0900

    simple implementations

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
 import './App.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import Board from './components/Board';
-import type { State } from './components/Types';
+import Button from './components/Button';
+import GameBoard from './components/GameBoard';
+import type { Board, State } from './components/Types';
+import { newBoard } from './utils/Functions';
 
 function App() {
+  useEffect(() => {
+    resetGame();
+  }, []);
+
+  const resetGame = () => {
+    const best = localStorage.getItem('best');
+    const board: Board = newBoard();
+
+    if (best === null) {
+      localStorage.setItem('best', '0');
+    }
+    setState({
+      score: 0,
+      bestScore: best === null ? 0 : parseInt(best),
+      board: board,
+    });
+  };
+
   const [state, setState] = useState<State>({
     score: 0,
     bestScore: 0,
     board: [
-      [2, 4, 8, 16], 
-      [32, 64, 128, 256],
-      [512, 0, 0, 0],
-      [0, 0, 0, 0]
-    ]
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
   });
 
   return (
     <>
-      <div className='game-title'>
-        2048 GAME
+      <div className="game-title">2048 GAME</div>
+      <br />
+      <div className="container">
+        <div className="score">
+          <span className="style-text">SCORE</span>
+          <span className="style-num">{state.score}</span>
+        </div>
+        <div className="score">
+          <span className="style-text">BEST</span>
+          <span className="style-num">{state.bestScore}</span>
+        </div>
+        <Button text="New Game" onClick={resetGame} />
       </div>
-      <br/>
-      <div>
-
-      </div>
-      <Board state={state} setState={setState}/>
+      <br />
+      <div></div>
+      <GameBoard state={state} setState={setState} />
     </>
   );
 }
--- src/components/Types.ts
+++ src/components/Types.ts
@@ -1,6 +1,11 @@
-export type Cell = number;
+type Cell = number;
+export type CellPos = {
+  x: number;
+  y: number;
+};
+export type Board = Cell[][];
 export type State = {
-    score: number;
-    bestScore: number;
-    board: Cell[][];
-}
+  score: number;
+  bestScore: number;
+  board: Board;
+};
